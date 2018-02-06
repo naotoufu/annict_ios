ffe29e65e773017fd41570e57e62ef5baafa12e7
伊東直人 <itounaoto@naotos-mac-mini.local>
2018-02-06 23:58:26 +0900

    Initial Commit

---
+++ Annict/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Annict
+//
+//  Created by 伊東直人 on 2018/02/06.
+//  Copyright © 2018 伊東直人. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
